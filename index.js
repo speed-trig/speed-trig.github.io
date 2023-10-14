ed0fb8e7c69fc49e6102507e8a55aea29b8dfe14
IceWizard20 <kailash.sabnis@gmail.com>
2023-10-14 18:11:18 -0400

    First real version of the product. Needs fixing

---
+++ index.js
@@ -0,0 +1,147 @@
+
+class Question{
+
+    constructor(angle, answer){
+
+        this.angle = angle
+        this.answer = answer
+
+    }
+
+
+}
+
+
+const sinQuestions = [
+    new Question(0, "0"),
+    new Question(30, "1/2"),
+    new Question(45, "√2/2"),
+    new Question(60, "√3/2"),
+    new Question(90, "1"),
+    new Question(120, "√3/2"),
+    new Question(135, "√2/2"),
+    new Question(150, "1/2"),
+    new Question(180, "0"),
+    new Question(210, "-1/2"),
+    new Question(225, "-√2/2"),
+    new Question(240, "-√3/2"),
+    new Question(270, "-1"),
+    new Question(300, "-√3/2"),
+    new Question(315, "-√2/2"),
+    new Question(330, "-1/2")
+]
+
+validQuestions = sinQuestions
+validAnswers = setAnswers(validQuestions);
+
+function setAnswers(questions){
+
+   let answers = [];
+
+    for(let i = 0; i < questions.length; i++){
+
+        if(!answers.includes(questions[i].answer)){
+            answers.push(questions[i].answer)
+        }
+    }
+    return answers
+}
+
+function selectQuestion(){
+    return validQuestions[Math.floor(Math.random() * validQuestions.length)]
+}
+
+function setButtons(question, answers){
+
+    let possibleAnswers = []
+    possibleAnswers.push(question.answer)
+    possibleAnswers.push(answers[Math.floor(Math.random() * answers.length)])
+    answers.filter(item => !possibleAnswers.includes(item))
+    possibleAnswers.push(answers[Math.floor(Math.random() * answers.length)])
+    answers.filter(item => !possibleAnswers.includes(item))
+    possibleAnswers.push(answers[Math.floor(Math.random() * answers.length)])
+
+    for (let i = possibleAnswers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [possibleAnswers[i], possibleAnswers[j]] = [possibleAnswers[j], possibleAnswers[i]];
+    }
+
+    document.getElementById("button0").innerHTML = possibleAnswers[0]
+    document.getElementById("button1").innerHTML = possibleAnswers[1]
+    document.getElementById("button2").innerHTML = possibleAnswers[2]
+    document.getElementById("button3").innerHTML = possibleAnswers[3]
+   
+}
+
+function displayQuestion(question)
+{   
+    let text = "Sin(" + question.angle + ")"
+    document.getElementById("question").innerHTML = text
+}
+
+let myQuestion = selectQuestion()
+setButtons(myQuestion, validAnswers)
+displayQuestion(myQuestion)
+
+
+
+document.getElementById("button0").onclick = function(question){
+
+    let buttonAnswer = document.getElementById("button0").value
+    let correctAnswer = question.answer
+
+    if(buttonAnswer == correctAnswer){
+        document.getElementById("correct").innerHTML = "correct"
+    }
+    else{
+        document.getElementById("correct").innerHTML = "incorrect"
+    }
+}
+
+document.getElementById("button1").onclick = function(question){
+
+    let buttonAnswer = document.getElementById("button1").value
+    let correctAnswer = question.answer
+
+    if(buttonAnswer == correctAnswer){
+        document.getElementById("correct").innerHTML = "correct"
+    }
+    else{
+        document.getElementById("correct").innerHTML = "incorrect"
+    }
+}
+
+document.getElementById("button2").onclick = function(question){
+
+    let buttonAnswer = document.getElementById("button2").value
+    let correctAnswer = question.answer
+
+    if(buttonAnswer == correctAnswer){
+        document.getElementById("correct").innerHTML = "correct"
+    }
+    else{
+        document.getElementById("correct").innerHTML = "incorrect"
+    }
+}
+
+document.getElementById("button3").onclick = function(question){
+
+    let buttonAnswer = document.getElementById("button3").value
+    let correctAnswer = question.answer
+
+    if(buttonAnswer == correctAnswer){
+        document.getElementById("correct").innerHTML = "correct"
+    }
+    else{
+        document.getElementById("correct").innerHTML = "incorrect"
+    }
+}
+
+
+document.getElementById("continue").onclick = function(){
+    myQuestion = selectQuestion()
+    setButtons(myQuestion, validAnswers)
+    displayQuestion(myQuestion)
+
+}
+
